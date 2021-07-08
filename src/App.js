acb348c0519072bc94968f8b61f543a81a5b52d8
ivantrj <ivantrajanovski@gmail.com>
2021-07-08 14:45:24 +0200

    add cards and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey = '324395a69ea6efa387196bbe9b63e0a42e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([])
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
-                    console.log(articles)
+                    setNewsArticles(articles)
                 }
             }
         })
@@ -20,6 +24,7 @@ const App = () => {
     return (
         <div>
             <h1>Alan AI News App</h1> 
+            <NewsCards articles={newsArticles} />
         </div>
     )
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewsCard from '../NewsCard/NewsCard';
+
+const NewsCards = ({ articles }) => {
+    <div>
+        News Card
+    </div>
+}
+
+export default NewsCard;
